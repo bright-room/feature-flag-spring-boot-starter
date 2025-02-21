0e31ac2c2c0f2a5048cf1c38e3c316f68717d39d
kukv <rio.by.kukv@gmail.com>
2025-02-21 22:24:19 +0900

    add gradle module

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "feature-flag-spring-boot-starter"
