e2eaf2018ad326d798e0810b658b2b433334d275
kukv <rio.by.kukv@gmail.com>
2025-02-24 14:38:07 +0900

    Added Spring MVC integration library

---
+++ web/src/main/java/net/brightroom/featureflag/configuration/FeatureFlagMvcAutoConfiguration.java
@@ -0,0 +1,22 @@
+package net.brightroom.featureflag.configuration;
+
+import net.brightroom.featureflag.interceptor.FeatureFlagInterceptor;
+import net.brightroom.featureflag.provider.FeatureFlagProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Autoconfiguration for Feature Flag MVC support. Configures the Feature Flag interceptor for
+ * Spring MVC applications. This configuration is loaded after the main Feature Flag
+ * autoconfiguration.
+ */
+@AutoConfigureAfter(FeatureFlagAutoConfiguration.class)
+public class FeatureFlagMvcAutoConfiguration {
+
+  @Bean
+  FeatureFlagInterceptor featureFlagInterceptor(FeatureFlagProvider featureFlagProvider) {
+    return new FeatureFlagInterceptor(featureFlagProvider);
+  }
+
+  FeatureFlagMvcAutoConfiguration() {}
+}
