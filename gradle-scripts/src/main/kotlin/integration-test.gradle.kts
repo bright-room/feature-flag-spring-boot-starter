92ae856d5fd2c00a14d9f7611bd6724c3eed70b9
kukv <koki-nonaka@outlook.jp>
2026-02-28 00:28:31 +0900

    integrationTestをconversion plugin化

---
+++ gradle-scripts/src/main/kotlin/integration-test.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+}
+
+sourceSets {
+    create("integrationTest") {
+        compileClasspath += sourceSets.main.get().output
+        runtimeClasspath += sourceSets.main.get().output
+    }
+}
+
+val integrationTestImplementation by configurations.getting {
+    extendsFrom(configurations.testImplementation.get())
+}
+
+val integrationTestRuntimeOnly by configurations.getting {
+    extendsFrom(configurations.testRuntimeOnly.get())
+}
+
+tasks.register<Test>("integrationTest") {
+    description = "Runs integration tests."
+    group = "verification"
+    testClassesDirs = sourceSets["integrationTest"].output.classesDirs
+    classpath = sourceSets["integrationTest"].runtimeClasspath
+    useJUnitPlatform()
+    shouldRunAfter(tasks.test)
+}
+
+tasks.check {
+    dependsOn(tasks.named("integrationTest"))
+}
